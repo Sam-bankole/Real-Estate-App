1b396ba76a9ffa7f16d8125c1b0019c214c3f8e6
Sam-bankole <sbankole495@gmail.com>
2024-02-07 03:25:53 +0100

    to share

--- src/Homepage/Testinmonials.jsx
+++ src/Homepage/Testinmonials.jsx
@@ -1,10 +1,113 @@
 import React from "react";
+import "./homepage.css"
 
 const Testimony = () => {
   return (
-    <div>
-      <div>
-        
+    <div className=" mt-20">
+        <div className=" px-8 text-3xl font-bold text-neutral-700">
+            See What People Say About Real Estate
+        </div>
+        <div className=" px-8">
+            Experience our seamless services
+        </div>
+      <div className=" p-4 flex justify-between items-center overflow-x-scroll gap-3 invisible-scrollbar">
+        {/* items */}
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex items-center border-2 border-zinc-300 p-2  bg-slate-50">
+          <div className="">
+            <img src="" alt="WriterImage" className=" h-12 w-12 rounded-full" />
+          </div>
+          <div className=" w-72 text-xs ml-3">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+            iure asperiores harum vel illo culpa vero itaque nemo soluta."
+            <div className=" font-semibold mt-2">
+              Name <span>Location</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
